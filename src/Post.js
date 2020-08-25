46836d416f0b37ef1eb56ba6d19f3ffdef9f6fd8
Bharat Poptwani <b4bharat.available@gmail.com>
2020-08-25 17:58:53 +0530

    completed the edit functionality

--- src/Post.js
+++ src/Post.js
@@ -7,7 +7,16 @@ class Post extends Component {
       <div>
         <h2>{this.props.post.title}</h2>
         <p>{this.props.post.message}</p>
-        <button>Edit</button>
+        <button
+          onClick={() =>
+            this.props.dispatch({
+              type: "EDIT_POST",
+              id: this.props.post.id
+            })
+          }
+        >
+          Edit
+        </button>
         <button
           onClick={() =>
             this.props.dispatch({
